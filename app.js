2c23110843f3fed2286f42c2cdc3bd8bdb97ba73
BotchaVarun <varunbotcha@gmail.com>
2025-04-18 12:04:46 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const Registration = require('./models/Registration');
+const Registration = require('../models/Registration');
 require('dotenv').config();
 
 const app = express();
